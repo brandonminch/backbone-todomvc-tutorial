657d6c6230821b77d58993b5de0ff2891d749720
Brandon Minch <brandonminch@gmail.com>
2013-02-22 15:34:31 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/*global $*/
+/*jshint unused:false*/
+var app = app || {};
+var ENTER_KEY = 13;
+
+$(function () {
+	'use strict';
+
+	// Kick things off by creating the **App**.
+	new app.AppView();
+
+});
