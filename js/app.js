dd18d387b2ab0afc9b3b61de5307da4f0223f838
Brandon Minch <brandonminch@gmail.com>
2013-03-07 14:37:41 -0700

    Creating starting point for Ex04 JS

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
 (function ($) {
 	'use strict';
 
-	// Ex3.0: Define the javascript object literal named App that will contain all of the application logic.
+	var App = {
+		'init': function() {
+			this.$newTodo = $('#new-todo');
 
-	// Ex3.1: Execute the App.init method.
+			this.bindEvents();
 
-	// Ex3.2 - 3.x: See README.md
+			console.log('App: init');
+		},
+
+		'bindEvents': function() {
+			this.$newTodo.on('keyup', this.create); // calls to the function below
+		},
+
+		'create': function(event) { // 'event' refers to event from above ('keyup')
+			if (event.which == '13') { // use 'which' instead of keyCode for cross-browser support
+				var value = App.$newTodo.val();
+
+				$('#todo-list').append('<li>' + value + '</li>');
+
+				App.$newTodo.val(''); // upon <enter>, clear value from new-todo input
+			}
+		}
+	};
+
+	App.init(); // Initialize the application
 
 }(jQuery));
